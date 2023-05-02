24f1493988c1f5468cd5e2cdbf7dfe49791798b1
sudhir kumar <sudhir.kumar@netcracker.com>
2023-05-03 00:01:35 +0530

    Initial commit

---
+++ src/main/java/com/sudhir/renderspringboot/RenderspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sudhir.renderspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenderspringbootApplication.class, args);
+    }
+
+}
